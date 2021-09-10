adf796154ca0359acaa97bf5705031676fd1c4ee
Nachiket Gavad <nachiketgavad@gmail.com>
2021-09-10 06:44:37 +0530

    day2

--- day2.c
+++ day2.c
@@ -2,12 +2,12 @@
 #include <math.h>
 int main()
 {
-    double meal_cost;
+    float meal_cost;
     int tip_percent, tax_percent, tip1, tax1;
-    double tip, tax, total_cost;
+    float tip, tax, total_cost;
 
     //meal cost
-    scanf("%lf", &meal_cost);
+    scanf("%f", &meal_cost);
 
     //tip_percent
     scanf("%d", &tip_percent);
@@ -25,8 +25,7 @@ int main()
 
 
     //total cost
-    total_cost = meal_cost + tip + tax;
-    printf("%d",(int)round(total_cost));
+    printf("%d",(int)round(meal_cost + tip + tax));
 
     return 0;
 }
